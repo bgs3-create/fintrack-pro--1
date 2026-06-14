4d589dd4141454eb8c1e0d8f7e5d2a67414e97dd
bgs3-create <bagasardiyansahxtjkt3@gmail.com>
2026-06-14 16:53:45 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images:
-                    {remotePatterns:[],
-                    },
-                  webpack: (config)=>{
-                    config.rosolve.alias.canvas = false
-                    return config
-                  },
-                   }
-module.exports=nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [],
+  },
+
+  webpack: (config) => {
+    config.resolve.alias.canvas = false
+    return config
+  },
+}
+
+module.exports = nextConfig
